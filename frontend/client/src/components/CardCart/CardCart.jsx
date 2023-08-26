ef98107277562721ca77f4862c4dc3f5713215d5
Tiaguh <tiagosingup@gmail.com>
2023-08-26 19:25:01 -0300

    fixing bugs in cart component

--- frontend/client/src/components/CardCart/CardCart.jsx
+++ frontend/client/src/components/CardCart/CardCart.jsx
@@ -40,7 +40,7 @@ export default function CardCart(props) {
                     className="remove-button"
                     onClick={props.onHandleDelete}
                 >
-                    Remove
+                    Remover
                 </button>
             </div>
 
--- frontend/client/src/components/CardMenu/CardMenu.jsx
+++ frontend/client/src/components/CardMenu/CardMenu.jsx
@@ -5,10 +5,10 @@ export default function ItemMenu(props) {
   return (
     <div className="card" key={props.id}>
       {props.item_image && <img src={props.item_image} alt="snack" />}
-      <h2>{props.item_name}</h2>
-      <p>{props.item_description}</p>
-      <h3>R$ {props.item_price}</h3>
-      <button onClick={props.onAddToCart}>Buy</button>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <h3>R$ {props.price}</h3>
+      <button onClick={props.onAddToCart}>Comprar</button>
     </div>
   )
 }
