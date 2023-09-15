95740ef87a4decbaa302b683696f1ffd9052380c
Tiaguh <tiagosingup@gmail.com>
2023-09-15 18:08:49 -0300

    consuming route to get all requests

--- frontend/admin/src/Pages/Requests/Requests.jsx
+++ frontend/admin/src/Pages/Requests/Requests.jsx
@@ -1,63 +1,64 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Header from '../../components/Header/Header'
-import Title from '../../components/Title/Title'
-import './Requests.css'
+import React, { useState, useEffect } from 'react';
+import api from '../../api.js';
+import Header from '../../components/Header/Header';
+import Title from '../../components/Title/Title';
+import './Requests.css';
 
 export default function Requests() {
+  const [items, setItems] = useState({ requests: [] });
 
-  const [items, setItems] = useState([])
+  console.log(items);
 
   useEffect(() => {
     const fetchAllItems = async () => {
       try {
-        const res = await axios.get("http://localhost:3333/select-requests")
-        setItems(res.data)
+        const res = await api.get("/requests/get-all-requests");
+        setItems(res.data);
       } catch (error) {
         console.log(error);
       }
-    }
-    fetchAllItems()
-  }, [])
+    };
 
-  const handleDelete = async (id_request) => {
-    try {
-      await axios.delete("http://localhost:3333/delete-request/" + id_request)
-      window.location.reload()
-    } catch (error) {
-      console.log(error);
-    }
-  }
+    fetchAllItems();
+  }, []);
+
+  const headerHeight = items.requests && items.requests.length < 4 ? "100vh" : "100%";
 
   return (
     <div className='requests-all-container'>
-      <Header height="100vh" />
 
-      <Title title="Requests" />
+      <Header height={headerHeight} />
 
       <div className="requests-container">
-        {items.map(item => (
-          <div className="requestsList-cards" key={item.id_request}>
+
+        <Title title="Requests" />
+
+        {items.requests.map(item => (
+
+          <div className="requestsList-cards" key={item.id}>
+
             <div className="request-id">
-              <h1>{item.id_request}</h1>
+              <h1>{item.id}</h1>
+            </div>
+
+            <div className="request-name">
+              <p>{item.name}</p>
             </div>
 
             <div className="request-describe">
               <h3>Item Request:</h3>
-              <p>{item.item_request}</p>
-              <h3>Method Pay:</h3>
-              <p>{item.methodo_pay}</p>
-              <h3>Address:</h3>
-              <p>{item.address}</p>
+              <p>{item.description}</p>
+
+              <p>Quantidade: {item.quantity}</p>
+
+              <p>{item.date}</p>
             </div>
 
-            <div className="request-delete">
-              <button onClick={() => handleDelete(item.id_request)}>Delete</button>
-            </div>
+
           </div>
         ))}
+
       </div>
     </div>
-  )
+  );
 }
