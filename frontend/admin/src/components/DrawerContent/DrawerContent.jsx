3bdc1d7f2a481d9c9f630f25c5f1ec38f54decc4
Tiaguh <tiagosingup@gmail.com>
2023-10-24 23:29:02 -0300

    customizing drawer

--- frontend/admin/src/components/DrawerContent/DrawerContent.jsx
+++ frontend/admin/src/components/DrawerContent/DrawerContent.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import "./DrawerContent.css"
 
-import { AiOutlineClose } from 'react-icons/ai'
+import HomeIcon from "./img/icons/home.png";
+import RequestIcon from "./img/icons/menu.png";
+import SettingsIcon from "./img/icons/settings.png";
+
+import CloseIcon from "./img/close.png";
+
+import { Link } from "react-router-dom";
+
+import { toast } from 'react-toastify';
 
 export default function DrawerContent({ setIsOpen }) {
     return (
         <div className="drawer-content-container">
 
-            <div className="drawer-content-button-container">
+            <div className="close-drawer">
 
                 <button
                     onClick={() => setIsOpen(false)}
@@ -15,8 +23,61 @@ export default function DrawerContent({ setIsOpen }) {
                     color="#FFF"
                     style={{ cursor: "pointer" }}
                 >
-                    <AiOutlineClose color="#FFF" size={50} />
+                    <img src={CloseIcon} alt="Close" />
                 </button>
+
+            </div>
+
+            <div>
+
+                <div className="side-bar">
+
+                    <Link className="link" to="/">
+                        <img src={HomeIcon} alt="Home" />
+                        <p>Home</p>
+                    </Link>
+
+                    <Link className="link" to="/requests">
+                        <img src={RequestIcon} alt="Requests" />
+                        <p>Requests</p>
+                    </Link>
+
+                    <Link className="link" to="/management-items">
+                        <img src={SettingsIcon} alt="Management Items" />
+                        <p>Management Items</p>
+                    </Link>
+
+                    <Link className="link" to="/menu">
+                        <img src={RequestIcon} alt="See Menu" />
+                        <p>See Menu</p>
+                    </Link>
+
+                </div>
+
+                <div className="logout">
+
+                    <Link className="link" to="/login">
+
+                        <button onClick={() => {
+                            sessionStorage.removeItem("login")
+                            sessionStorage.clear();
+
+                            toast.success('Deslogado com sucesso!', {
+                                position: "top-right",
+                                autoClose: 5000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                                theme: "dark",
+                            });
+                        }}>
+                            Logout
+                        </button>
+                    </Link>
+
+                </div>
 
             </div>
 
