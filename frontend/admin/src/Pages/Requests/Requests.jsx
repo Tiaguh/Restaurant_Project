72556a1c5461b17721e77cbef63800da11293d6d
Tiaguh <tiagosingup@gmail.com>
2023-11-30 22:06:22 -0300

    trying to improve the order screen

--- frontend/admin/src/Pages/Requests/Requests.jsx
+++ frontend/admin/src/Pages/Requests/Requests.jsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
-
 import api from '../../api.js';
-
-import Drawer from '../../components/Drawer/Drawer.jsx'
+import Drawer from '../../components/Drawer/Drawer.jsx';
 import Title from '../../components/Title/Title';
-
 import './Requests.css';
 
 export default function Requests() {
   const [items, setItems] = useState({ requests: [] });
-
-  console.log(items);
+  const [userItems, setUserItems] = useState([]);
 
   useEffect(() => {
     const fetchAllItems = async () => {
@@ -25,46 +21,58 @@ export default function Requests() {
     fetchAllItems();
   }, []);
 
-  // const headerHeight = items.requests && items.requests.length < 4 ? "100vh" : "100%";
+  useEffect(() => {
+    const groupedItems = items.requests.reduce((acc, item) => {
+      const userIndex = acc.findIndex(user => user.user_name === item.user_name);
+
+      if (userIndex !== -1) {
+        // Se o usuário já existe, adiciona o item à lista existente
+        acc[userIndex].items.push({
+          itemName: item.name,
+          description: item.description,
+          quantity: item.quantity,
+        });
+      } else {
+        // Se o usuário não existe, cria um novo objeto de usuário com um array contendo o item
+        acc.push({
+          user_name: item.user_name,
+          items: [{
+            itemName: item.name,
+            description: item.description,
+            quantity: item.quantity,
+          }],
+        });
+      }
+
+      return acc;
+    }, []);
+
+    setUserItems(groupedItems);
+  }, [items.requests]);
 
   return (
     <div className='requests-all-container'>
-
       <Drawer />
-
       <div className="requests-container">
-
         <Title title="Requests" />
 
-        {items.requests.map(item => (
-
-          <div className="requestsList-cards" key={item.id}>
-
-            <div className="request-id">
-              <h1>{item.id}</h1>
-            </div>
-
-            <div className="request-name">
-              <p>{item.name}</p>
-            </div>
-
-            <div className="request-describe">
-              <h3>Item Request:</h3>
-              <p>{item.description}</p>
-
-              <p>Quantidade: {item.quantity}</p>
-
-              {item.date && (
-                <p>{new Date(item.date).toISOString().split('T')[0]}</p>
-              )}
-
-              <p>{item.hour}</p>
-            </div>
-
-
+        {userItems.map(user => (
+          <div key={user.user_name} className="requestsList-cards">
+            <h2>{user.user_name}</h2>
+            {user.items.map((item, index) => (
+              <div key={index}>
+                <div className="request-name">
+                  <p>{item.itemName}</p>
+                </div>
+                <div className="request-describe">
+                  <h3>Item Request:</h3>
+                  <p>{item.description}</p>
+                  <p>Quantidade: {item.quantity || 'N/A'}</p>
+                </div>
+              </div>
+            ))}
           </div>
         ))}
-
       </div>
     </div>
   );
