866730d6b91c6b963d4646c47811833c29a00fca
Tiaguh <tiagosingup@gmail.com>
2023-12-02 13:15:05 -0300

    consuming route and fixing bugs

--- frontend/admin/src/Pages/Requests/Requests.jsx
+++ frontend/admin/src/Pages/Requests/Requests.jsx
@@ -8,13 +8,15 @@ export default function Requests() {
   const [items, setItems] = useState({ requests: [] });
   const [userItems, setUserItems] = useState([]);
 
+  console.log(items);
+
   useEffect(() => {
     const fetchAllItems = async () => {
       try {
         const res = await api.get("/requests/get-all-requests");
         setItems(res.data);
       } catch (error) {
-        console.log(error);
+        console.error("Erro ao buscar os pedidos:", error);
       }
     };
 
@@ -23,24 +25,22 @@ export default function Requests() {
 
   useEffect(() => {
     const groupedItems = items.requests.reduce((acc, item) => {
-      const userIndex = acc.findIndex(user => user.user_name === item.user_name);
+      const user = acc.find((user) => user.user_name === item.user_name);
 
-      if (userIndex !== -1) {
-        // Se o usuário já existe, adiciona o item à lista existente
-        acc[userIndex].items.push({
-          itemName: item.name,
-          description: item.description,
-          quantity: item.quantity,
+      if (user) {
+        user.items.push({
+          item_name: item.item_requests.split(' ')[0],
+          quantity: parseInt(item.item_requests.split(' ')[1]) || 1, // Extrai a quantidade ou assume 1 se não houver
         });
       } else {
-        // Se o usuário não existe, cria um novo objeto de usuário com um array contendo o item
         acc.push({
           user_name: item.user_name,
-          items: [{
-            itemName: item.name,
-            description: item.description,
-            quantity: item.quantity,
-          }],
+          items: [
+            {
+              item_name: item.item_requests.split(' ')[0],
+              quantity: parseInt(item.item_requests.split(' ')[1]) || 1,
+            },
+          ],
         });
       }
 
@@ -52,27 +52,37 @@ export default function Requests() {
 
   return (
     <div className='requests-all-container'>
+
       <Drawer />
+
       <div className="requests-container">
+
         <Title title="Requests" />
 
-        {userItems.map(user => (
-          <div key={user.user_name} className="requestsList-cards">
-            <h2>{user.user_name}</h2>
-            {user.items.map((item, index) => (
-              <div key={index}>
-                <div className="request-name">
-                  <p>{item.itemName}</p>
+        {userItems.length > 0 ? (
+          userItems.map((user, userIndex) => (
+
+            <div key={userIndex} className="request-card">
+
+              <h1>{user.user_name}</h1>
+              <h2>Items Requests:</h2>
+
+              {user.items.map((item, index) => (
+
+                <div key={index} className="requests-info">
+
+                  <h2>{item.item_name}</h2>
+                  <h3>Quantidade: {item.quantity || 'N/A'}</h3>
+                  
                 </div>
-                <div className="request-describe">
-                  <h3>Item Request:</h3>
-                  <p>{item.description}</p>
-                  <p>Quantidade: {item.quantity || 'N/A'}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
+
+              ))}
+            </div>
+
+          ))
+        ) : (
+          <p>Nenhum pedido encontrado.</p>
+        )}
       </div>
     </div>
   );
