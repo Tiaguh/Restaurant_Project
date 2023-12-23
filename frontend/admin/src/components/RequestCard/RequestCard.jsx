99c32486a43f78521ae785c65b6604bdd0e84490
Tiaguh <tiagosingup@gmail.com>
2023-12-23 16:21:06 -0300

    fixing bugs in component

--- frontend/admin/src/components/RequestCard/RequestCard.jsx
+++ frontend/admin/src/components/RequestCard/RequestCard.jsx
@@ -4,19 +4,29 @@ import "./RequestCard.css";
 import { FaCheck } from "react-icons/fa";
 
 export default function RequestCard({ request }) {
+  const parseItems = (itemsString) => {
+    return itemsString.split(', ').map(item => {
+      const [itemName, quantity] = item.split(' (');
+      return { itemName, quantity: parseInt(quantity.replace(')', ''), 10) };
+    });
+  };
+
+  const itemsArray = parseItems(request.items);
+
   return (
     <div className="request-card">
-      
+
       <h1>{request.id_request}</h1>
       <h2>{request.user_name}</h2>
 
       <h3>Items Requests:</h3>
-      
-      {request.items && (
-        <div className="requests-info">
-          <h2>{request.items}</h2>
+
+      {itemsArray.map((item, index) => (
+        <div key={index} className="requests-info">
+          <h4>{item.itemName}</h4>
+          <h5>Quantidade: {item.quantity || 'N/A'}</h5>
         </div>
-      )}
+      ))}
 
       <div className="request-button-container">
         <button className="apply">
