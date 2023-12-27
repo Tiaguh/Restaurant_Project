8bb6063c4c913833d57bf5a95df9d33f991e8e3f
Tiaguh <tiagosingup@gmail.com>
2023-12-27 18:26:26 -0300

    consuming api

--- frontend/admin/src/Pages/Home/Home.jsx
+++ frontend/admin/src/Pages/Home/Home.jsx
@@ -3,11 +3,14 @@ import './Home.css';
 
 import Title from '../../components/Title/Title';
 import Drawer from '../../components/Drawer/Drawer';
+import api from '../../api.js';
 
 export default function Home() {
   const [hour, setHour] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+
+  const [report, setReport] = useState(null);
 
   const currentTime = () => {
     const data = new Date();
@@ -28,14 +31,41 @@ export default function Home() {
     };
   }, []);
 
+  useEffect(() => {
+    const fetchReports = async () => {
+      try {
+        const res = await api.get("/report/get-reports");
+        setReport(res.data.reports);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchReports();
+  }, []);
+
   return (
     <div className='management-container'>
+
       <Drawer />
+
+      <Title title="Home" />
+
       <div className='management-main'>
-        <Title title="Home" />
+
         <div className="clock-container">
           <h1>{hour} : {minutes} : {seconds}</h1>
         </div>
+
+
+        <div className="report">
+          {report && (
+            <>
+              <h2>Lucro Total: R$ {report.sales}</h2>
+              <h2>Total de Itens Vendidos: {report.sold}</h2>
+            </>
+          )}
+        </div>
+
       </div>
     </div>
   );
