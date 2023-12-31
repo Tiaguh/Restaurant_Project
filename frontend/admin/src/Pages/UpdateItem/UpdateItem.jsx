62aba46c74d67a42f9be8f478b09acadc02f9dfa
Tiaguh <tiagosingup@gmail.com>
2023-12-31 15:13:47 -0300

    fixing bugs in update item

--- frontend/admin/src/Pages/UpdateItem/UpdateItem.jsx
+++ frontend/admin/src/Pages/UpdateItem/UpdateItem.jsx
@@ -1,109 +1,118 @@
-import React, { useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-
-import './UpdateItem.css'
-
-import Menu from './img/menu.png'
-import Drawer from '../../components/Drawer/Drawer'
-import Title from '../../components/Title/Title'
-
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './UpdateItem.css';
+import Menu from './img/menu.png';
+import Drawer from '../../components/Drawer/Drawer';
+import Title from '../../components/Title/Title';
 import { toast } from 'react-toastify';
-import api from '../../api.js'
+import api from '../../api.js';
 
 export default function UpdateItem() {
-
   const [item, setItem] = useState({
-    itemName: "",
-    itemDescription: "",
+    itemName: '',
+    itemDescription: '',
     itemPrice: null
-  })
-
-  const navigate = useNavigate()
-  const location = useLocation() // pegar o id do item baseado na rota em que ele está
-
-  const itemId = (location.pathname.split("/")[2]);
-
-  const handleChange = (e) => {
-    setItem((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  }
+  });
 
   console.log(item);
 
+  const navigate = useNavigate();
+  const location = useLocation();
+  const itemId = location.pathname.split('/')[2];
+
+  console.log(itemId);
+
+  const handleChange = (e) => {
+    setItem((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   const handleClick = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const res = await api.post(`/management-item/update-item/${itemId}`, item)
-      console.log(res);
+      const res = await api.post(`/management-item/update-item/${itemId}`, item);
 
       if (res.status === 200) {
-        navigate("/");
+        navigate('/');
         toast.success('Atualizado com sucesso!', {
-          position: "top-right",
+          position: 'top-right',
           autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "dark",
+          theme: 'dark'
         });
       }
 
-      navigate("/alter-items")
-
+      navigate('/alter-items');
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      try {
+        if (itemId) {
+          const response = await api.get(`/management-item/get-item/${itemId}`);
+          console.log(response.data);
+
+          if (response.data) {
+            const { name, description, price } = response.data;
+            setItem({
+              itemName: name,
+              itemDescription: description,
+              itemPrice: price
+            });
+          }
+        }
+      } catch (error) {
+        console.log('API Error:', error);
+      }
+    };
+    fetchItemDetails();
+  }, [itemId]);
 
   return (
-    <div className='update-all-item-container'>
-
+    <div className="update-all-item-container">
       <Drawer />
-
       <div className="update-item-container">
-
         <Title title="Update a Item To The Menu" />
-
         <div className="update-item-main">
-
           <div className="update-item-main-img">
-            <img src={Menu} alt='Menu' />
+            <img src={Menu} alt="Menu" />
           </div>
-
           <form>
-
             <input
-              className='input'
+              className="input"
               type="text"
-              placeholder='Item Name'
+              placeholder="Item Name"
+              value={item.itemName}
               onChange={handleChange}
-              name='itemName'
+              name="itemName"
             />
-
             <input
-              className='input'
+              className="input"
               type="text"
-              placeholder='Item Description'
+              placeholder="Item Description"
+              value={item.itemDescription}
               onChange={handleChange}
-              name='itemDescription'
+              name="itemDescription"
             />
-
             <input
-              className='input'
+              className="input"
               type="number"
-              placeholder='Item Price'
+              placeholder="Item Price"
+              value={item.itemPrice}
               onChange={handleChange}
-              name='itemPrice'
+              name="itemPrice"
             />
-
             <button onClick={(e) => handleClick(e)}>Update</button>
-
           </form>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
