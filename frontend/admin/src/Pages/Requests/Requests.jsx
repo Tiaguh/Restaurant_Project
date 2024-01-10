ae0095aafb41b4563f454d5e1411e62300d333a7
Tiaguh <tiagosingup@gmail.com>
2024-01-10 09:48:45 -0300

    improving interface

--- frontend/admin/src/Pages/Requests/Requests.jsx
+++ frontend/admin/src/Pages/Requests/Requests.jsx
@@ -6,22 +6,52 @@ import Title from '../../components/Title/Title';
 import RequestCard from '../../components/RequestCard/RequestCard.jsx';
 
 import api from '../../api.js';
+import { toast } from 'react-toastify';
 
 export default function Requests() {
   const [requests, setRequests] = useState([]);
 
-  useEffect(() => {
-    const fetchAllRequests = async () => {
-      try {
-        const res = await api.get("/requests/get-all-requests");
-        setRequests(res.data.requests);
-      } catch (error) {
-        console.error("Erro ao buscar os pedidos:", error);
-      }
-    };
+  const fetchAllRequests = async () => {
+    try {
+      const res = await api.get("/requests/get-all-requests");
+      setRequests(res.data.requests);
+    } catch (error) {
+      console.error("Erro ao buscar os pedidos:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchAllRequests();
   }, []);
+
+  const handleFinishRequest = async (id_request) => {
+    try {
+      await api.put(`/requests/finalize-request/${id_request}`);
+      fetchAllRequests();
+      toast.success('Pedido finalizado com sucesso!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } catch (err) {
+      console.log(err);
+      toast.error('Erro ao finalizar o pedido', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
 
   return (
     <div className='requests-all-container'>
@@ -31,19 +61,20 @@ export default function Requests() {
       <div className="requests-container">
 
         <Title title="Requests" />
-        
+
         {requests.length > 0 ? (
           requests.map((request, index) => (
             <RequestCard
               key={index}
               request={request}
+              handleFinishRequest={handleFinishRequest}
             />
           ))
         ) : (
           <p>Nenhum pedido pendente.</p>
         )}
       </div>
-      
+
     </div>
   );
 }
