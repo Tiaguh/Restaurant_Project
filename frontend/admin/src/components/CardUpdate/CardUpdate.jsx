e2ffb02b481f03230876e828ec4c805ea45f708f
Tiago Alves <101520829+Tiaguh@users.noreply.github.com>
2024-01-14 10:38:14 -0300

    fixing lag bugs

--- frontend/admin/src/components/CardUpdate/CardUpdate.jsx
+++ frontend/admin/src/components/CardUpdate/CardUpdate.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import './CardUpdate.css'
+import './CardUpdate.css';
 
-export default function CardUpdate(props) {
-    return (
-        <div key={props.id} className="card">
+export default function CardUpdate({ handleUpdate, ...props }) {
+  return (
+    <div key={props.id} className="card">
 
-            <div className="card-update-info">
-                <h2>{props.name}</h2>
-                <p>{props.description}</p>
-                <h3>R$ {props.price}</h3>
-            </div>
+      <div className="card-update-info">
+        <h2>{props.name}</h2>
+        <p>{props.description}</p>
+        <h3>R$ {props.price}</h3>
+      </div>
 
-            <div className="card-update-button"> 
-                <button>
-                    <Link className='link' to={`/update-item/${props.id}`}>Update</Link>
-                </button>
-
-                <button onClick={props.onDelete}>Delete</button>
-            </div>
-        </div>
-    )
+      <div className="card-update-button">
+        <button onClick={() => handleUpdate(props.id)}>
+          <Link className="link" to={`/update-item/${props.id}`}>
+            Update
+          </Link>
+        </button>
+        <button onClick={props.onDelete}>Delete</button>
+      </div>
+    </div>
+  );
 }
