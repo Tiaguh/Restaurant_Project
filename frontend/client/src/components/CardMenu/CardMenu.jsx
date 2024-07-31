e246f54ec7c227d03dc0438869ac43e5d9334e68
Tiaguh <tiagosingup@gmail.com>
2024-07-30 21:45:17 -0300

    refactoring card menu component

--- frontend/client/src/components/CardMenu/CardMenu.jsx
+++ frontend/client/src/components/CardMenu/CardMenu.jsx
@@ -3,12 +3,22 @@ import "./CardMenu.css";
 
 export default function CardMenu(props) {
   return (
-    <div className="card" key={props.id}>
-      {props.image_url && <img src={props.image_url} alt={props.name} />}
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-      <h3>R$ {props.price}</h3>
-      <button onClick={props.onAddToCart}>Comprar</button>
+    <div className="card-menu" key={props.id}>
+
+      <div className="card-menu-img">
+        {props.image_url && <img src={props.image_url} alt={props.name} />}
+      </div>
+
+      <div className="card-menu-description">
+        <h2>{props.name}</h2>
+        <p>{props.description}</p>
+        <h3>R$ {props.price}</h3>
+      </div>
+
+      <div className="card-menu-button">
+        <button onClick={props.onAddToCart}>Comprar</button>
+      </div>
+
     </div>
   );
 }
